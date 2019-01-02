a722b01af918b4dafcc359ef141bb4c53ecf91ed
Bradley Bossard <bradleybossard@gmail.com>
2019-01-02 14:24:16 -0600

    Compile JS with babel

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ var moment = require('moment');
 console.log("Hello from JavaScript!");
 
 console.log(moment().startOf('day').fromNow());
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
   output: {
     filename: 'main.js',
     publicPath: 'dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
