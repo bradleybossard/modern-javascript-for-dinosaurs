17096508819a4250d6a168f040c7d17028866334
Bradley Bossard <bradleybossard@gmail.com>
2019-01-05 11:36:38 -0600

    Add webpack-dev-server and scripts

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ console.log("Hello from JavaScript!");
 
 console.log(moment().startOf('day').fromNow());
 
-var name = "Rob", time = "today";
+var name = "Bill", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
